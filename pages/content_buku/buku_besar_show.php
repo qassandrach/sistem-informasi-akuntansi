a37fff282b628f9b5d09243c6df46265426c6883
qassandrach <qassiechaidir@gmail.com>
2021-01-08 20:48:29 +0700

    responsive coa & bukubesar

--- pages/content_buku/buku_besar_show.php
+++ pages/content_buku/buku_besar_show.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include '../config/configuration.php';
 
 $nama_akun = $_POST['nama_akun'];
@@ -7,77 +7,71 @@ $nama_akun = $_POST['nama_akun'];
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="?content=home">Home</a></li>
     <li class="breadcrumb-item"><a href="?content=buku_besar">Buku Besar</a></li>
-    <li class="breadcrumb-item">Buku Besar <?php echo $nama_akun?></li>
+    <li class="breadcrumb-item">Buku Besar <?php echo $nama_akun ?></li>
   </ol>
- <br>
- <center><p style="font-size:160%;font-weight: bold">Buku Besar <?php echo "$nama_akun"?> </p></center>
- <div class="col-md-10" style="min-height:600px">
+  <br>
 
-            <table class="table table-bordered">
+  <div class="col-12">
+    <center>
+      <p style="font-size:160%;font-weight: bold">Buku Besar <?php echo "$nama_akun" ?> </p>
+    </center>
+    <div class="card">
+      <div class="table-responsive">
+        <table class="table">
+          <thead class="thead-light">
+            <tr>
+              <th class="info">Tanggal Transaksi</th>
+              <th class="info">Nama Transaksi</th>
+              <th class="info">Debit</th>
+              <th class="info">Kredit</th>
+              <th class="info">Saldo</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            include '../config/configuration.php';
+            $saldo = 0;
+            $gol = '';
+            $sql_akun = mysqli_query($conn, "SELECT * FROM tb_coa where nama='$nama_akun'");
+            while ($data_akun = mysqli_fetch_assoc($sql_akun)) {
+              $gol = $data_akun['gol'];
+            }
+            $transaksi = mysqli_query($conn, "SELECT tgl, nama_transaksi, CASE WHEN debet='$nama_akun' THEN nominal END AS debit, CASE WHEN kredit='$nama_akun' THEN nominal END AS kredit FROM transaksi WHERE debet='$nama_akun' OR kredit='$nama_akun'");
+            while ($data_transaksi = mysqli_fetch_assoc($transaksi)) {
+              $tgl = $data_transaksi['tgl'];
+              $nama_transaksi = $data_transaksi['nama_transaksi'];
+              $saldo_debit = $data_transaksi['debit'];
+              $saldo_kredit = $data_transaksi['kredit'];
+
+              if ($gol == 'Debet') {
+                if ($saldo_kredit == 0) {
+                  $saldo = $saldo + $saldo_debit;
+                } else {
+                  $saldo = $saldo - $saldo_kredit;
+                }
+              } else {
+                if ($saldo_kredit == 0) {
+                  $saldo = $saldo - $saldo_debit;
+                } else {
+                  $saldo = $saldo + $saldo_kredit;
+                }
+              }
+
+            ?>
               <tr>
-                <th class="info">Tanggal Transaksi</th>
-                <th class="info">Nama Transaksi</th>
-                <th class="info">Debit</th>
-                <th class="info">Kredit</th>
-                <th class="info">Saldo</th>
-              </tr>
-              <?php
-              include '../config/configuration.php';
-                $saldo = 0;
-                $gol = '';
-                $sql_akun = mysqli_query($conn, "SELECT * FROM tb_coa where nama='$nama_akun'");
-                while ($data_akun = mysqli_fetch_assoc($sql_akun)) {
-                  $gol = $data_akun['gol'];
-                }
-                $transaksi = mysqli_query($conn, "SELECT tgl, nama_transaksi, CASE WHEN debet='$nama_akun' THEN nominal END AS debit, CASE WHEN kredit='$nama_akun' THEN nominal END AS kredit FROM transaksi WHERE debet='$nama_akun' OR kredit='$nama_akun'");
-                while($data_transaksi = mysqli_fetch_assoc($transaksi)) {
-                  $tgl = $data_transaksi['tgl'];
-                  $nama_transaksi = $data_transaksi['nama_transaksi'];
-                  $saldo_debit = $data_transaksi['debit'];
-                  $saldo_kredit = $data_transaksi['kredit'];
-                  
-                  if ($gol == 'Debet') {
-                    if ($saldo_kredit == 0){
-                      $saldo = $saldo + $saldo_debit;
-                    } else {
-                      $saldo = $saldo - $saldo_kredit;
-                    }
-                  } else {
-                    if ($saldo_kredit == 0){
-                      $saldo = $saldo - $saldo_debit;
-                    } else {
-                      $saldo = $saldo + $saldo_kredit;
-                    }
+                <td> <?php echo $tgl ?></th>
+                <td> <?php echo $nama_transaksi ?></th>
+                <td> <?php echo number_format($saldo_debit) ?></th>
+                <td> <?php echo number_format($saldo_kredit) ?></th>
+                <td> <?php echo number_format($saldo) ?></th>
+              </tr><?php
+
                   }
-                 
-                  ?>
-                  <tr>
-                    <td> <?php echo $tgl?></th>
-                    <td> <?php echo $nama_transaksi?></th>
-                    <td> <?php echo number_format($saldo_debit)?></th>
-                    <td> <?php echo number_format($saldo_kredit)?></th>
-                    <td> <?php echo number_format($saldo)?></th>
-                  </tr><?php
+                    ?>
 
-                }
-              ?>
-            </table>
-            <div class="col-md-12">
-               <nav align="center">
-                 <ul class="pagination">
-                   <li>
-                     <a href="#" aria-label="Previous">
-                       <span aria-hidden="true">&laquo;</span>
-                     </a>
-                   </li>
-                   <li><a href="#">1</a></li>
-                   <li>
-                     <a href="#" aria-label="Next">
-                       <span aria-hidden="true">&raquo;</span>
-                     </a>
-                   </li>
-                 </ul>
-               </nav>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 
-            </div>
-   </div>
--- pages/content_coa/coa.php
+++ pages/content_coa/coa.php
@@ -1,56 +1,49 @@
-
- <nav aria-label="breadcrumb">
+<nav aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="?content=home">Home</a></li>
     <li class="breadcrumb-item">Code of Account</li>
   </ol>
 </nav>
- <div class="col-md-10" style="min-height:600px">
-         <div class="col-md-12" style="padding:10px; padding-left:0;padding-right:0;">
-            <a href="?content=coa_tambah" class="btn btn-info">Tambah</a>
-         </div>
-            <table class="table table-bordered">
-               <tr>
-                  <th class="info">Kode Akun</th>
-                  <th class="info">Nama Akun</th>
-				  <th class="info">Golongan</th>
-				  <th class="info">Klasifikasi</th>
-				  <th class="info">Saldo</th>
-                  <th class="info" colspan="2">Action</th>
-               </tr>
-			   <?php
-    include '../config/configuration.php';
-    $product = mysqli_query($conn, "SELECT * from tb_coa");
-    foreach ($product as $result){
-		echo "<tr>
-				<td>".$result['kode_akun']."</td>
-				<td>".$result['nama']."</td> 
-				<td>".$result['gol']."</td> 
-				<td>".$result['klas']."</td> 
-				<td>RP. ".number_format($result['saldo'])."</td> 
+<div class="col-12">
+  <center>
+    <p style="font-size:160%;font-weight: bold">Code of Account</p>
+  </center>
+  <div class="col-md-12" style="padding:10px; padding-left:0;padding-right:0;">
+    <a href="?content=coa_tambah" class="btn btn-info">Tambah</a>
+  </div>
+
+  <div class="card">
+    <div class="table-responsive">
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th class="info">Kode Akun</th>
+            <th class="info">Nama Akun</th>
+            <th class="info">Golongan</th>
+            <th class="info">Klasifikasi</th>
+            <th class="info">Saldo</th>
+            <th class="info" colspan="2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          include '../config/configuration.php';
+          $product = mysqli_query($conn, "SELECT * from tb_coa");
+          foreach ($product as $result) {
+            echo "<tr>
+				<td>" . $result['kode_akun'] . "</td>
+				<td>" . $result['nama'] . "</td> 
+				<td>" . $result['gol'] . "</td> 
+				<td>" . $result['klas'] . "</td> 
+				<td>RP. " . number_format($result['saldo']) . "</td> 
 				<td><a href='?content=coa_edit&kode_akun=$result[kode_akun]'>edit</a></td>
                 <td><a href='?content=coa_delete&kode_akun=$result[kode_akun]'>delete</a></td>
 				</tr>";
-	}
-		
-		?>
-            </table>
-            <div class="col-md-12">
-               <nav align="center">
-                 <ul class="pagination">
-                   <li>
-                     <a href="#" aria-label="Previous">
-                       <span aria-hidden="true">&laquo;</span>
-                     </a>
-                   </li>
-                   <li><a href="#">1</a></li>
-                   <li>
-                     <a href="#" aria-label="Next">
-                       <span aria-hidden="true">&raquo;</span>
-                     </a>
-                   </li>
-                 </ul>
-               </nav>
+          }
 
-            </div>
-   </div>
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
